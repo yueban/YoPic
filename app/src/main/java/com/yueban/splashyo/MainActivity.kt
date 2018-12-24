abdbccc5597c0480af82f75423fd0623f9180c60
yueban <fbzhh007@gmail.com>
2018-12-24 17:30:53 +0800

    apply codeStyle and reformat code; fix warnings in Manifest.xml

--- app/src/main/java/com/yueban/splashyo/MainActivity.kt
+++ app/src/main/java/com/yueban/splashyo/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.yueban.splashyo
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
