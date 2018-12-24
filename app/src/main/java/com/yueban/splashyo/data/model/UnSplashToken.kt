3aead5a1d0f6cc35f028fa0655ad1cee9d50796a
yueban <fbzhh007@gmail.com>
2018-12-24 19:27:20 +0800

    complete application structure design & create packages

---
+++ app/src/main/java/com/yueban/splashyo/data/model/UnSplashToken.kt
@@ -0,0 +1,8 @@
+package com.yueban.splashyo.data.model
+
+/**
+ * @author yueban
+ * @date 2018/12/24
+ * @email fbzhh007@gmail.com
+ */
+data class UnSplashToken(val access_key: String, val secret_key: String)
---
+++ app/src/main/java/com/yueban/splashyo/util/Utils.kt
@@ -0,0 +1,12 @@
+package com.yueban.splashyo.util
+
+import android.content.Context
+
+/**
+ * @author yueban
+ * @date 2018/12/24
+ * @email fbzhh007@gmail.com
+ */
+fun getUnSplashToken(context: Context) {
+
+}
